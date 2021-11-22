17af9b8c6f0405f7b5924aab200a608b1729e618
Matty-D-123 <matthewdurick@gmail.com>
2021-11-22 21:15:19 +1000

    Updated the launcher icon, MapsActivity.java markers are currently commented out.

--- app/src/main/java/matthewdurick/com/example/mymap/MapsActivity.java
+++ app/src/main/java/matthewdurick/com/example/mymap/MapsActivity.java
@@ -64,38 +64,102 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .snippet("long click to drag")
                 .draggable(true)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        personMark.setTag("start");
 
         // Add a marker on LINQ Precinct
         LatLng linq = new LatLng(-27.568670, 153.233669);
         Marker linqMark = mMap.addMarker(new MarkerOptions()
                 .position(linq)
                 .title("LINQ Precinct"));
+        linqMark.setTag("end");
 
         // Add a marker on the Admin Building
         LatLng admin = new LatLng(-27.57038, 153.23420);
         Marker adminMark = mMap.addMarker(new MarkerOptions()
                 .position(admin)
                 .title("Admin Building"));
+        adminMark.setTag("end");
 
-        // Add a marker on the SLC
+        /*// Add a marker on the SLC
         LatLng slc = new LatLng(-27.56999, 153.23409);
         Marker slcMark = mMap.addMarker(new MarkerOptions()
                 .position(slc)
                 .title("SLC"));
+        slcMark.setTag("end");
 
         // Add a marker on the Pavilion
         LatLng pavil = new LatLng(-27.56979, 153.23363);
         Marker pavilMark = mMap.addMarker(new MarkerOptions()
                 .position(pavil)
                 .title("Pavilion and Cafe 97"));
+        pavilMark.setTag("end");
+
+        // Add a marker on the English and Humanities Block
+        LatLng enghum = new LatLng(-27.56939, 153.23426);
+        Marker enghumMark = mMap.addMarker(new MarkerOptions()
+                .position(enghum)
+                .title("English and Humanities Block"));
+        enghumMark.setTag("end");
+
+        // Add a marker on the Art Block
+        LatLng art = new LatLng(-27.56945, 153.23380);
+        Marker artMark = mMap.addMarker(new MarkerOptions()
+                .position(art)
+                .title("Art Block"));
+        artMark.setTag("end");
+
+        // Add a marker on the Science Block
+        LatLng sci = new LatLng(-27.56958, 153.23322);
+        Marker sciMark = mMap.addMarker(new MarkerOptions()
+                .position(sci)
+                .title("Science Block"));
+        sciMark.setTag("end");
+
+        // Add a marker on the Math Block
+        LatLng mat = new LatLng(-27.56919, 153.23338);
+        Marker matMark = mMap.addMarker(new MarkerOptions()
+                .position(mat)
+                .title("Math Block"));
+        matMark.setTag("end");
+
+        // Add a marker on the Stadium
+        LatLng stad = new LatLng(-27.56950, 153.23259);
+        Marker stadMark = mMap.addMarker(new MarkerOptions()
+                .position(stad)
+                .title("Stadium"));
+        stadMark.setTag("end");
+
+        // Add a marker on the Performing Arts Block
+        LatLng pa = new LatLng(-27.57024, 153.23295);
+        Marker paMark = mMap.addMarker(new MarkerOptions()
+                .position(pa)
+                .title("Performing Arts Block"));
+        paMark.setTag("end");
+
+        // Add a marker on the Uniform Shop
+        LatLng uni = new LatLng(-27.57037, 153.23384);
+        Marker uniMark = mMap.addMarker(new MarkerOptions()
+                .position(uni)
+                .title("Uniform Shop"));
+        uniMark.setTag("end");
+
+        // Add a marker on the Music Rooms
+        LatLng mus = new LatLng(-27.56989, 153.23295);
+        Marker musMark = mMap.addMarker(new MarkerOptions()
+                .position(mus)
+                .title("Music Rooms"));
+        musMark.setTag("end");
+
+        // Add a marker on the Year 6 Block
+        LatLng yr6 = new LatLng(-27.57021, 153.23330);
+        Marker yr6Mark = mMap.addMarker(new MarkerOptions()
+                .position(yr6)
+                .title("Year 6 Block"));
+        yr6Mark.setTag("end");*/
 
         mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
             @Override
             public void onMarkerDragStart(Marker marker) {
-                linqMark.hideInfoWindow(); //Hides title of LINQ marker when blue marker is dragged
-                adminMark.hideInfoWindow(); //Hides title of Admin marker when blue marker is dragged
-                slcMark.hideInfoWindow(); //Hides title of Admin marker when blue marker is dragged
-                pavilMark.hideInfoWindow(); //Hides title of Admin marker when blue marker is dragged
 
             }
 
@@ -112,31 +176,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker){
-                if (marker.equals(linqMark)){
-                    Double distance; // Creates a double variable called distance
-                    distance = SphericalUtil.computeDistanceBetween(personMark.getPosition(), marker.getPosition()); // Sets distance variable as distance between blue marker location and linq marker
-                    int easyDistance = (int) Math.round(distance); // Rounds the distance variable to an int (makes it easier to read in a toast)
-                    toastMsg("Distance to " + marker.getTitle() + ": " + easyDistance + "m"); //Brings up toast message that displays the int distance
-                    marker.showInfoWindow();
-                    return true;
-                }
-                if (marker.equals(adminMark)){
-                    Double distance; // Creates a double variable called distance
-                    distance = SphericalUtil.computeDistanceBetween(personMark.getPosition(), marker.getPosition()); // Sets distance variable as distance between blue marker location and admin marker
-                    int easyDistance = (int) Math.round(distance); // Rounds the distance variable to an int (makes it easier to read in a toast)
-                    toastMsg("Distance to " + marker.getTitle() +": " + easyDistance + "m"); //Brings up toast message that displays the int distance
-                    marker.showInfoWindow(); //Show the title of the clicked marker
-                    return true;
-                }
-                if (marker.equals(slcMark)){
-                    Double distance; // Creates a double variable called distance
-                    distance = SphericalUtil.computeDistanceBetween(personMark.getPosition(), marker.getPosition()); // Sets distance variable as distance between blue marker location and admin marker
-                    int easyDistance = (int) Math.round(distance); // Rounds the distance variable to an int (makes it easier to read in a toast)
-                    toastMsg("Distance to " + marker.getTitle() +": " + easyDistance + "m"); //Brings up toast message that displays the int distance
-                    marker.showInfoWindow(); //Show the title of the clicked marker
-                    return true;
-                }
-                if (marker.equals(pavilMark)) {
+                Object markertag = marker.getTag(); //creates a variable called markertag that is the tag of the clicked marker
+
+                if (markertag.equals("end")) {
                     Double distance; // Creates a double variable called distance
                     distance = SphericalUtil.computeDistanceBetween(personMark.getPosition(), marker.getPosition()); // Sets distance variable as distance between blue marker location and admin marker
                     int easyDistance = (int) Math.round(distance); // Rounds the distance variable to an int (makes it easier to read in a toast)
@@ -144,7 +186,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     marker.showInfoWindow(); //Show the title of the clicked marker
                     return true;
                 }
-                return false;
+                else {
+                    marker.showInfoWindow(); //Show the title of the clicked marker
+                    return true;
+                }
             }
         });
         
